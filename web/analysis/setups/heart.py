46b04894c928d77c6d0a4c337e699ae1f84bee11
Alex Berman <alex@timebend.net>
2023-06-07 14:26:39 +0200

    Heart: Fix bug in python version of algorithm

--- web/analysis/setups/heart.py
+++ web/analysis/setups/heart.py
@@ -6,6 +6,6 @@ def predict(case_info):
         (case_info['ca'] in ['1.0', '2.0', '3.0'])
     ]
     print(f'condition_evaluations={condition_evaluations}')
-    num_true_conditions = len([value for value in condition_evaluations if value is True])
+    num_true_conditions = len([value for value in condition_evaluations if value])
     print(f'num_true_conditions={num_true_conditions}')
     return int(num_true_conditions >= 2)
